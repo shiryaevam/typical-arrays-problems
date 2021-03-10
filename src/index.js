fdd2f4e8aa19bbbd32090697d104fdede35b5d6d
Aleksey.Shiryaev <Aleksey.Shiryaev@payever.org>
2021-03-11 00:05:27 +0400

    feat: test 100%

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
+    let min = 0;
+    if(array === 0|| array === undefined){
+        return min = 0
+    }else {
+        for(let i = 0; i < array.length; i++){
+            if(array[i]<min){
+                min = array[i];
+            }else{
+                min
+            }
+    }
+
+    }
+
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let max = 0;
+    if(array === 0 || array === undefined){
+        return max = 0
+    }else {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            } else {
+                max
+            }
+        }
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+        let avg = 0;
+    if(array === [] || array === undefined || array.length === 0){
+        return 0;
+    }else {
+        for (let i = 0; i < array.length; i++) {
+            avg += array[i];
+        }
+        return avg/array.length;
+    }
 }
+
+
